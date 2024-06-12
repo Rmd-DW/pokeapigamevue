870dc5ac14517b73127328c2daf65273eefb3855
Rmd-DW <r.munizaga.d@gmail.com>
2024-06-11 21:39:51 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="cont-app">
+    <img src="/logo.png" alt="Pokemon Logo" width="250px">
+    <h1>¿Quién es ese Pokémon?</h1>
+    <img class="iconPoke" src="/pokeball_gray.png" alt="" width="50px">
+    <h2>Total descubiertos: {{ discoveredCount }}</h2>
+    <div v-if="pokemones.length > 0">
+      <div class="grilla-pokemon">
+        <pokemonCard 
+          :pokemon="pokemon" 
+          v-for="pokemon in pokemones" 
+          :key="pokemon.name" 
+          @pokemon-discovered="incrementDiscoveredCount"
+        />
+      </div>
+    </div>
+    <div v-else>
+      <p v-if="error">Error al cargar los Pokémon. Por favor, inténtalo de nuevo más tarde.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import pokemonCard from '@/components/pokemonCard.vue';
+
+export default {
+  components: {
+    pokemonCard,
+  },
+  data() {
+    return {
+      urlBase: 'https://pokeapi.co/api/v2/pokemon',
+      pokemones: [],
+      error: null,
+      discoveredCount: 0,
+    };
+  },
+  methods: {
+    async getPokemones() {
+      try {
+        const { data } = await axios.get(`${this.urlBase}?limit=20`);
+        const listaPokemones = data.results;
+
+        const promises = listaPokemones.map(async (pokemon, index) => {
+          const { data } = await axios.get(`${this.urlBase}/${index + 1}/`);
+          const { name, sprites: { other: { dream_world: { front_default } } } } = data;
+
+          return {
+            name,
+            img: front_default,
+          };
+        });
+
+        this.pokemones = await Promise.all(promises);
+      } catch (error) {
+        this.error = error.message || "Error al cargar los Pokémon";
+      }
+    },
+    incrementDiscoveredCount() {
+      this.discoveredCount += 1;
+    },
+  },
+  mounted() {
+    this.getPokemones();
+  }
+};
+</script>
+
+<style>
+body {
+  display: flex;
+  justify-content: center;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  background-image: url("https://assets.pokemon.com/static2/_ui/img/chrome/body_bg.png");
+  color: white;
+}
+
+h1 {
+  font-size: 61px;
+  color: white;
+}
+
+.cont-app {
+  text-align: center;
+  margin: 1rem;
+  padding: 1rem;
+}
+
+
+
+.grilla-pokemon {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.iconPoke {
+  filter: brightness(100%);
+}
+</style>
---
+++ src/components/pokemonCard.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="container">
+    <div class="pokemon-card">
+      <h3 v-show="isPokemonNameDiscovered">{{ pokemon.name }}</h3>
+      <img :src="pokemon.img" :class="{ 'no-blur': isPokemonDiscovered }" alt="">
+      <input :class="{ 'no-show': isPokemonDiscovered }" v-model="pokemonName" type="text">
+      <button :class="{ 'no-show': isPokemonDiscovered }"  @click="showPokemones">Descubrir</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      pokemonName: "",
+      isPokemonDiscovered: false,
+      isPokemonNameDiscovered: false,
+    };
+  },
+  props: {
+    pokemon: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    showPokemones() {
+      if (this.pokemonName === this.pokemon.name) {
+        this.isPokemonNameDiscovered = true;
+        this.isPokemonDiscovered = true;
+        this.$emit('pokemon-discovered'); 
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  margin: 0.5rem;
+  padding: 0.5rem;
+}
+
+.pokemon-card {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin: 0.5rem;
+  padding: 0.5rem;
+  background-color: white;
+  color: rgb(53, 53, 53);
+  border-radius: 10px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
+  width: 250px;
+  height: 350px;
+}
+
+img {
+  margin: 1rem;
+  padding: 1rem;
+  width: 150px;
+  height: 150px;
+  filter: blur(10px) grayscale(100%);
+}
+
+img.no-blur {
+  filter: none;
+}
+
+
+
+
+span {
+  color: black;
+}
+
+input {
+  padding: 0.3rem;
+  margin: 0.3rem;
+}
+
+input.no-show{
+  display: none;
+}
+
+button {
+  padding: 0.3rem;
+  margin: 0.3rem;
+  background-color: #085898;
+  color: white;
+  border-radius: 10px;
+}
+
+button.no-show{
+  display: none;
+}
+</style>
